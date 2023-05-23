56cbe4aa5ebe55550d17a8ac214fbeff308b900d
Emerson Okorie <emersonokorie@icloud.com>
2023-05-23 19:01:54 -0300

    update

---
+++ JAVASCRIPT/DOM/blank.js
@@ -0,0 +1,6 @@
+//grab the document obj module
+var headOne = document.querySelector("#one")
+var headTwo = document.querySelector("#two")
+var headThree = document.querySelector("#three")
+
+console.log("connected")
