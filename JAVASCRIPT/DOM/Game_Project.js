1ae4eea30d12ed7db6739cf61e2ab9cba87d821c
Emerson Okorie <emersonokorie@icloud.com>
2023-05-29 09:44:16 -0300

    update

--- JAVASCRIPT/DOM/Game_Project.js
+++ JAVASCRIPT/DOM/Game_Project.js
@@ -12,19 +12,21 @@ function clearBoard(){
 }
 restart.addEventListener("click", clearBoard)
 
+
+
 //Check the square marker
+
 function changeMarker(){
-    if(this.textContent === " "){
-        this.textContent === "X"
-        }else if (this.textContent == "X"){
-            this.textContent === "O"
-        }else{
-            this.textContent === " "
+    if(this.textContent === ""){
+        this.textContent = "X"
+        }else if (this.textContent === "X"){
+            this.textContent = "O"
+         }else{this.textContent = ""
         }
-    }
+         }
 
 
-// For Loop to add event listeners to all the squares
+// // For Loop to add event listeners to all the squares
 
-for (var i = 0; k < squares.length; i++){
+for (var i = 0; i < squares.length; i++){
     squares[i].addEventListener("click", changeMarker)}
